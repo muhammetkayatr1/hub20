c1b39b55d588b654d2d54ea3ac786a633631cd81
muhammetkayatr1 <69511438+muhammetkayatr1@users.noreply.github.com>
2020-10-20 11:56:01 +0300

    first commit

---
+++ HalicHub/Models/EMail/EMailModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalicHub.Models
+{
+    public class EMailModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+
+    }
+}
